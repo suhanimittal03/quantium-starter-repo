0255f47eaed977c5d3717b761a9ff14a23c39516
Suhani Mittal <suhani.mittal@iitgn.ac.in>
2024-05-25 14:59:01 +0530

    data sorted

---
+++ data_merge.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+
+sales_data_0 = pd.read_csv('data/daily_sales_data_0.csv')
+sales_data_1 = pd.read_csv('data/daily_sales_data_1.csv')
+sales_data_2 = pd.read_csv('data/daily_sales_data_2.csv')
+# print(sales_data_0.head())
+
+sales_data = pd.concat([sales_data_0, sales_data_1, sales_data_2])
+# print(sales_data)
+
+mask = sales_data['product'] == 'pink morsel'
+
+sales_data_copy = sales_data[mask]
+sales_data_copy.reset_index(inplace=True, drop=True)
+sales_data_copy['price'] = sales_data_copy['price'].replace({'\$': ''}, regex=True).astype(float).astype(int)
+sales_data_copy['date'] = pd.to_datetime(sales_data_copy['date'])
+print(sales_data_copy.dtypes)
+
+sales_data_copy['sales'] = sales_data_copy['price']*sales_data_copy['quantity']
+sales_data_copy = sales_data_copy.drop(['price', 'quantity', 'product'], axis=1)
+print(sales_data_copy.head())
+
+sales_data_copy.to_csv('sales_data.csv', index=False)
